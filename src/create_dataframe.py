0bcc223fe5a516c5736723e1ddc9f7be361bb0df
Pascal Reiss <pascal.reiss@students.fhnw.ch>
2023-03-25 18:39:53 +0100

    util.py works now and you can compare all numbers in the combined_data_set

--- src/create_dataframe.py
+++ src/create_dataframe.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 import pandas as pd
 import regex as re
+import os
+
 from wikipedia_crawler import getListOfMuncipalities
 
+from util import getPathDynamically
+from util import read_csv
+from util import create_csv
+
 def main() -> None:
-    muncipalities_general_path = Path('./raw_data/gemeinde_allgemein.xlsx')
-    if not muncipalities_general_path.exists():
-        raise FileNotFoundError(f'The file {muncipalities_general_path} could not be found.')
+    muncipalities_general_path = getPathDynamically('raw_data', 'gemeinde_allgemein.xlsx')
 
     df = pd.read_excel(muncipalities_general_path, header=None, skiprows=9, index_col=None, 
                        skipfooter=16, usecols=lambda column: column != 0, )
@@ -20,32 +24,22 @@ def main() -> None:
                                 'second_sector_Workplaces_total', 'third_sector_Workplaces_total', 'Empty_Appartments_Number',
                                 'New_homes_per_1000_inhabitants', 'Socialsecurity_in_Percent', 'FDP', 'CVP', 'SP', 'SVP', 'EVP_CSP',
                                 'GLP', 'BDP', 'PdA_Sol', 'GPS', 'Small_parties']
+    # In case the wikipedia_crawler wasnt run before i do not directly read the file containing the muncipalities as it could not exists yet in some casesd
     relevant_muncipalities = getListOfMuncipalities()
-
-    df['Gemeinde_Name'] = df['Gemeinde_Name'].apply(lambda x: re.sub(r'\p{C}', '', x))
-
-    # Here regex matching is used so () are an issue, interestingly it works with the newly added entry, but does not if I add the others it can not find
     df = df[df['Gemeinde_Name'].isin(relevant_muncipalities)]
 
-    abstimmung_path = Path('./raw_data/anderung-vom-19-maerz-2021-des-covid-19-gesetzes.csv')
-    if not abstimmung_path.exists():
-        raise FileNotFoundError(f'The file {muncipalities_general_path} could not be found.')
-    abstimmung_df = pd.read_csv(abstimmung_path)
+    abstimmung_df = read_csv('raw_data', 'anderung-vom-19-maerz-2021-des-covid-19-gesetzes.csv')
     abstimmung_df = abstimmung_df.rename(columns={'name':'Gemeinde_Name'})
    
-    famous_people_path = Path('./raw_data/famous_people.csv')
-    if not famous_people_path.exists():
-        raise FileNotFoundError(f'The file {famous_people_path} could not be found.')
-    famous_people_df = pd.read_csv(famous_people_path)
+    famous_people_df = read_csv('raw_data', 'famous_people.csv')
     famous_people_df_grouped = famous_people_df.groupby('Gemeinde_Name')['Famous_Person'].agg([('Famous_Person_list', lambda x: '|'.join(x)), ('Famous_Person_count', 'count')]).reset_index()
 
     dfs = [df, abstimmung_df, famous_people_df_grouped]
     big_boy_df = dfs[0]
     for df in dfs[1:]:
         big_boy_df = pd.merge(big_boy_df, df, on='Gemeinde_Name', how='outer')
-    big_boy_df_path = Path('./raw_data/combined_data_set.csv')
-    big_boy_df.to_csv(big_boy_df_path, mode='w', index=False)
 
+    create_csv('data', 'combined_data_set.csv', big_boy_df, index=False, overwrite=True)
 
 
 
--- src/wikipedia_crawler.py
+++ src/wikipedia_crawler.py
@@ -1,11 +1,14 @@
-import os
-import regex as re
+
 import requests
 import pandas as pd
 import wikipediaapi
 from bs4 import BeautifulSoup as bs
 from typing import List
-from pathlib import Path
+
+from util import getPathDynamically
+from util import read_csv
+from util import create_csv
+from util import create_csv_path
 
 wiki = wikipediaapi.Wikipedia(
     language='de',
@@ -13,7 +16,7 @@ wiki = wikipediaapi.Wikipedia(
 )
 
 def getListOfMuncipalities() -> List[str]:
-    municpalities_path = Path('./raw_data/muncipalities.csv')
+    municpalities_path = getPathDynamically('raw_data', 'muncipalities.csv', check_exists=False)
     municpality_df: pd.DataFrame
     if (municpalities_path.exists()):
         municpality_df = pd.read_csv(municpalities_path)
@@ -24,10 +27,10 @@ def getListOfMuncipalities() -> List[str]:
         muncipalities = []
         for tr in soup.find_all('td')[1::8]:
             a = tr.find('a')
-            muncipalities.append(re.sub(r'\p{C}', '', a.text))
+            muncipalities.append(a.text)
     
         municpality_df = pd.DataFrame(muncipalities, columns=['name'])
-        createFileFromDf(municpalities_path, municpality_df)
+        create_csv_path(municpalities_path, municpality_df)
     
     return municpality_df.name.tolist()
 
@@ -35,7 +38,7 @@ def getListOfMuncipalities() -> List[str]:
 
 
 def main() -> None:
-    famous_people_path = Path('./raw_data/famous_people.csv')
+    famous_people_path = getPathDynamically('raw_data', 'famous_people.csv', check_exists=False)
     if famous_people_path.exists():
         return
     muncipalities = getListOfMuncipalities()
@@ -53,25 +56,19 @@ def main() -> None:
                                   , 'Weitere Persönlichkeiten', '(Sortierung nach Geburtsjahr)', 'Kunst/Kultur:'
                                   , 'Politik und Unternehmertum:', 'Wissenschaft: ', 'Kunst/Kultur/Medien:', 'Sport: '
                                   , 'Politik und Unternehmertum: ', 'Söhne und Töchter der Stadt', 'Söhne und Töchter Maienfelds']:
-                    person = person.strip('"')
-                    print(person)
                     famous_person.append(person)
+                    # change all instances as the gemeinde_allgemein file uses another name
                     if (muncipality == 'Roveredo'):
                         muncipality = 'Roveredo (GR)'
                     muncipality_of_famous_person.append(muncipality)
 
     famous_people_df = pd.DataFrame({'Famous_Person': famous_person, 'Gemeinde_Name': muncipality_of_famous_person})
-    famous_people_path = Path('./raw_data/famous_people.csv')
-    createFileFromDf(famous_people_path, famous_people_df)
-    municpalities_path = Path('./raw_data/muncipalities.csv')
-    muncipalities_df = pd.read_csv(municpalities_path)
+    create_csv('raw_data', 'famous_people.csv', famous_people_df, index=True)
+
+    # after getting all muncipality pages from wikipedia we can now update it to match the name in all ofther files
+    muncipalities_df = read_csv('raw_data', 'muncipalities.csv')
     muncipalities_df.loc[muncipalities_df['name'] == 'Roveredo', 'name'] = 'Roveredo (GR)'
-    muncipalities_df.to_csv(municpalities_path, mode='w', index=False)
-
-def createFileFromDf(path: Path, df: pd.DataFrame, index=False) -> None:
-    if (not path.exists()):
-        df.to_csv(path, index=index)
-
+    create_csv('raw_data', 'muncipalities.csv', muncipalities_df, index=False, overwrite=True)
 
 
 main()
